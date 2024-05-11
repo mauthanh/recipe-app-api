92d942baf70b5187ac2ec8cbc2510f38f6354a08
Truong Mau Thanh <mauthanh@Truongs-MacBook-Pro.local>
2024-05-11 23:41:12 +0700

    add core app in settings

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ ENV PYTHONUNBUFFERED 1
 
 COPY ./requirements.txt /tmp/requirements.txt
 COPY ./app /app
+
 WORKDIR /app
 EXPOSE 8000
 
--- app/app/tests.py
+++ app/app/tests.py
@@ -1,25 +1,25 @@
-"""
-Test sample
-"""
-from django.test import SimpleTestCase
-
-from rest_framework.test import APIClient
-# from app import calc
-
-
-# class CalcTests(SimpleTestCase):
-#     def test_add_numbers(self):
-#         result = calc.add(5, 6)
-#         self.assertEqual(result, 11)
+# """
+# Test sample
+# """
+# from django.test import SimpleTestCase
 #
-#     def test_subtract_numbers(self):
-#         result = calc.subtract(15, 10)
-#         self.assertEqual(result, 5)
-
-class TestViews(SimpleTestCase):
-    def test_get_greeting(self):
-        client = APIClient()
-        result = client.get('/greetings/')
-
-        self.assertEqual(result.status_code, 200)
-        # self.assertEqual(result.data, ['Hello', 'Bonjour', 'Hola'])
+# from rest_framework.test import APIClient
+# # from app import calc
+#
+#
+# # class CalcTests(SimpleTestCase):
+# #     def test_add_numbers(self):
+# #         result = calc.add(5, 6)
+# #         self.assertEqual(result, 11)
+# #
+# #     def test_subtract_numbers(self):
+# #         result = calc.subtract(15, 10)
+# #         self.assertEqual(result, 5)
+#
+# class TestViews(SimpleTestCase):
+#     def test_get_greeting(self):
+#         client = APIClient()
+#         result = client.get('/greetings/')
+#
+#         self.assertEqual(result.status_code, 200)
+#         # self.assertEqual(result.data, ['Hello', 'Bonjour', 'Hola'])
